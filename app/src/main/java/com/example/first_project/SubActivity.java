41da42aa913de1f6b48a1261154469c8b890fba4
chris1797 <jaehun6004@gmail.com>
2022-10-29 17:11:34 +0900

    2022-10-29 Btn-Toast test

--- app/src/main/java/com/example/first_project/SubActivity.java
+++ app/src/main/java/com/example/first_project/SubActivity.java
@@ -2,7 +2,6 @@ package com.example.first_project;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -17,14 +16,16 @@ public class SubActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
 
-        Button Btn2 = findViewById(R.id.Btn2);
+        Button Btn2 = findViewById(R.id.Btn2Click);
         Btn2.setOnClickListener(new View.OnClickListener() {
             /**
              * 이걸로 log를 찍으면서 test할 수 있음, Logcat 창에서 확인 가능
              */
             @Override
             public void onClick(View view) {
-                Log.d("TAG", "VALUE");
+                Toast toast = Toast.makeText(view.getContext(), "zz", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.END | Gravity.BOTTOM, 10,10);
+                toast.show();
             }
         });
 
@@ -32,20 +33,19 @@ public class SubActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SubActivity.this, MainActivity.class);
-                intent.putExtra("키", "위");
+                intent.putExtra("키", "내용을 입력해주세요.");
                 startActivity(intent);
                 /**
                  * 현재 class인 SubActivity.this에서 시작, MainActivity class를 호출하면
                  * MainActivity의 onCreate()가 실행됨
                  * 그 다음 onCreate() 내 setContentView(R.layout.activity_main); 창이 뜸
                  * 그러면 MainActivity를 찾을 수 없다고 logcat에 뜸
-                 *
                  */
             }
         });
     }
 
-    public void Btn1Click(View view) { // activity_sub의 onClick이랑 메소드 이름이 같아야 함
+    public void Btn2Click(View view) { // activity_sub의 onClick이랑 메소드 이름이 같아야 함
         // Toast 메시지를 띄우는 작업
         Toast toast = Toast.makeText(this, "zz", Toast.LENGTH_LONG);
         toast.setGravity(Gravity.END | Gravity.BOTTOM, 10,10);
